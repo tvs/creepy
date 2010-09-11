e4bb4c37c1b96903b33ae8724c499b785fa0814e
bhadresh <bhadresh@wsu.edu>
2010-09-10 22:24:12 -0700

    URL validation and changed Fetcher

--- lib/creepy/Crawler.py
+++ lib/creepy/Crawler.py
@@ -7,6 +7,7 @@ import sys
 import time
 import os
 import re
+import urlparse
 try: # Try using psyco JIT compilation!
     import psyco
     psyco.full()
@@ -22,86 +23,88 @@ __authors__ = "Travis Hall <trvs.hll@gmail.com>, Brittany Miller <miller317@gmai
 __date__ = "Sep 5, 2010"
 __user_agent__ = "creepybot"
 
-_verbose = 2
+_verbose = False
 _debug = False
 
 class Crawler:
-    def __init__(self, seed, threshold=0):
+    """
+    A simple Web Crawler
+    """
+    def __init__(self, seeds, threshold=0):
         self.crawled = [] # Already Crawled URLs
-        self.queue = [] # Crawl Queue
+        self.frontier = [] # Crawler Request Queue
         self.threshold = int(threshold) # Max. Number of Pages to Crawl
         self.robotstorage = RobotStorage(__user_agent__)
-        for url in seed:
+        for url in seeds:
             self.queue_url(url)
 
     def crawl(self):
         """From the current queue start crawling pages"""
         url = self.get_next_url()
         while url != None:
+            if _verbose:
+                print "  Crawling:", url
             robot = self.robotstorage.get_robot(url)
-            if robot.is_allowed(url):
-                
+            if robot.is_allowed(url):                
                 # Delay processing
                 d_time = robot.delay_remaining(time.time())
                 if d_time > 0:
-                    if _verbose > 2:
+                    if _verbose > 1:
                         print "\n###*** Delaying for %f seconds" % d_time
                     time.sleep(d_time)
                 
-                if _verbose > 2:
-                    print "  Crawling:", url
                 self.crawled.append(url)
                 
                 # Update the last request time
                 robot.update_last_request(time.time())
                 
-                page = Fetcher(url)
-                doc = page.get_content()
-                p = Parser(url, doc)
-                links = p.get_links()
-                print "    # links found: ", len(links)
-                self.queue_links(links)
-
-                # Next
-                if self.threshold > 0 and len(self.crawled) >= self.threshold:
-                    if _verbose > 2:
-                        print "\n###*** Quitting... Threshold reached:", self.threshold, "***###\n"
-                    url = None
-                else:
-                    url = self.get_next_url()
-                
+                # Download the Page
+                page = Fetcher(url, verbose=_verbose)
+                doc = page.get_content()                
+                if doc:
+                    # @TODO: Store Document
+                    
+                    # Parse Page for Links
+                    p = Parser(url, doc)
+                    links = p.get_links()
+                    if _verbose:
+                        print "    # links found: ", len(links)
+                    self.queue_links(links)
             else:
-                if _verbose > 2:
+                if _verbose > 1:
                     print "*** URL not allowed:", url
+            
+            # Get Next URL
+            if self.threshold > 0 and len(self.crawled) >= self.threshold:
+                if _verbose:
+                    print "\n###*** Quitting... Threshold reached:", self.threshold, "***###\n"
+                url = None
+            else:
                 url = self.get_next_url()
-    
 
     def get_next_url(self):
         """Get Next url from the queue"""
-        return self.queue.pop(0) if len(self.queue) > 0 else None
+        return self.frontier.pop(0) if len(self.frontier) > 0 else None
 
     def queue_url(self, url):
-        """
-        Add url to the queue for crawling
-
-        @TODO: Add logic to verify url syntax & discard invalid url
-        """
-        if url not in self.crawled:
-            self.queue.append(url)
+        """Add url to the queue for crawling"""
+        if url not in self.crawled and self.validate_url(url):
+            self.frontier.append(url)
 
     def queue_links(self, links):
-        """
-        Add links to the queue for crawling
-
-        @TODO: Add logic to make link into URL
-        """
+        """Add links to the queue for crawling"""
         for link in links:
             self.queue_url(link)
 
+    def validate_url(self, url):
+        """Validate given url"""
+        u = urlparse.urlparse(url)
+        return u.scheme in ['http', 'https'] and u.netloc and not u.fragment
+        
 if __name__ == "__main__":
     import optparse
     parser = optparse.OptionParser(description="Web Crawler",
-                        usage="usage: %prog [options] <seedfile | seedurls>",
+                        usage="usage: %prog [options] <seedfile | seeds>",
                         version=__version__)
     parser.add_option('-v', '--verbose', help="Verbose Output [default: %default]", action="count", default=_verbose)
     parser.add_option('-d', '--debug', help="Debug Mode [default: %default]", action="count", default=_debug)
@@ -115,23 +118,23 @@ if __name__ == "__main__":
     _verbose = options.verbose
     _debug = options.debug
 
-    seed = []
+    seeds = []
     for arg in args:
         if os.path.isfile(arg):
             fp = open(args[0], "r")
             fcontent = fp.read()
             fp.close()
-            seed.extend(fcontent.splitlines())
+            seeds.extend(fcontent.splitlines())
         else:
-            seed.append(arg)
+            seeds.append(arg)
 
-    if len(seed) == 0:
+    if len(seeds) == 0:
         print "Please provide Seed URL or File that contains seed urls"
         parser.print_help()
         sys.exit(1)
 
-    print "### Number of seed URLs:", len(seed)
-    c = Crawler(seed, options.page_threshold)
+    print "### Number of seeds:", len(seeds)
+    c = Crawler(seeds, options.page_threshold)
     c.crawl()
     print "### Number of URLs crawled:", len(c.crawled)
-    print "### Number of URLs pending:", len(c.queue)
+    print "### Number of URLs pending:", len(c.frontier)
--- lib/creepy/Fetcher.py
+++ lib/creepy/Fetcher.py
@@ -9,27 +9,38 @@ import urllib2
 _headers = {"User-Agent": "creepybot"}
 
 class Fetcher:
-    def __init__(self, url):
+    def __init__(self, url, verbose=False):
         self.url = url
-    
-    def add_headers(self, request):
-        for key,value in _headers.iteritems():
-            request.add_header(key, value)
-    
-    
-    def get_content(self):
-        content = None
+        self.response = None
         try:
             request = urllib2.Request(self.url)
             handle = urllib2.build_opener()
             self.add_headers(request)
-            content = handle.open(request).read()
+            self.response = handle.open(request)            
         except urllib2.HTTPError, e:
-            print >> sys.stderr, "*** Error:", e
+            if verbose > 2:
+                print >> sys.stderr, "*** Error:", e
         except urllib2.URLError, e:
-            print >> sys.stderr, "*** Error: Invalid URL:", e
+            if verbose > 2:
+                print >> sys.stderr, "*** Error: Invalid URL:", e
         except:
-            print >> sys.stderr, "*** Error: Could not open url:", self.url
+            if verbose > 2:
+                print >> sys.stderr, "*** Error: Could not open url:", self.url        
+        
+    def add_headers(self, request):
+        for key,value in _headers.iteritems():
+            request.add_header(key, value)
+        
+    def get_content(self):        
+        content = None
+        try:
+            if self.response:
+                content = self.response.read()
+        except:
+            pass
         return content
+    
+    def get_response(self):
+        return self.response
     
 
