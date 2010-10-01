0324c06668fa8476f6081f9d8d3e1b1f7ca3403c
Travis Hall <trvs.hll@gmail.com>
2010-10-01 13:44:05 -0700

    Parser functionality added. Text Declaration [77] and subroutines added

--- lib/validator/parser.rb
+++ lib/validator/parser.rb
@@ -6,6 +6,7 @@ module Parser
   class XML_Syntax_Error < Exception; end
 
   class Parser
+    XML_DEFAULT_VERSION = "1.0"
     # XML Parser based heavily from libxml2
     # TODO: readers
     attr_accessor :index, :eof_index, :content, :line
@@ -19,7 +20,7 @@ module Parser
     end
   
   
-    # TODO: PRIVATE
+    # TODO: PRIVATIZE
     # Returns the current character in the string
     # TODO: Find a better place to make this check. Having to check each query is expensive
     def cur
@@ -57,16 +58,14 @@ module Parser
       i.times { @index += 1; @line += 1 if cur == "\n" }
     end
     
-    # skip_blank
+    # skip_blanks
     # skip XML whitespace
     #
     # [3] S ::= (#x20 | #x9 | #xD | #xA)+
     #
-    def skip_blank()
-      c = cur[0]
-      while (c == 0x20 || c == 0x9 || c = 0xD || c = 0xA)
+    def skip_blanks
+      while is_blank?
         skip
-        c = cur[0]
       end
     end
     
@@ -74,7 +73,7 @@ module Parser
     # returns true if cur is a blank char
     def is_blank?
       c = cur[0]
-      return c == 0x20 || c == 0x9 || c = 0xD || c = 0xA
+      return c == 0x20 || c == 0x9 || c == 0xD || c == 0xA
     end
     
     # is_char?
@@ -84,7 +83,13 @@ module Parser
       return (((0x9 <= (c)) && ((c) <= 0xA)) || ((c) == 0xD) || (0x20 <= (c)))
     end
     
-    # xmlParseComment:
+    # move_to_end_tag
+    # Moves the current index until it reaches a '>'
+    def move_to_end_tag
+      while (cur != '>'); skip; end
+    end
+    
+    # xmlParseComment
     # Skip an XML comment
     # For compatibility, the string "--" (double-hyphen) must not occur within comments.
     #
@@ -95,7 +100,7 @@ module Parser
       skip(4)
       begin
         until (cmp("-->"))
-          raise XML_Comment_Error, "Line #{@line}: '--' must not occur within comments" if cmp("--")
+          raise XML_Comment_Error, "line #{@line}: '--' must not occur within comments" if cmp("--")
           skip # Skip forward until we find a closing brace
         end
         skip(3) # Skip the "-->"
@@ -104,7 +109,7 @@ module Parser
       end
     end
   
-    # xmlParsePI:
+    # xmlParsePI
     # parse an XML processing instruction
     #
     # [16] PI ::= '<?' PITarget (S (Char* - (Char* '?>' Char*)))? '?>'
@@ -114,7 +119,7 @@ module Parser
         skip(2)
         target = xmlParsePITarget()
         
-        raise XML_Syntax_Error, "Line #{@line}: PI #{target}, whitespace expected" if !is_blank?
+        raise XML_Syntax_Error, "line #{@line}: PI #{target}, whitespace expected" if !is_blank?
         
         skip_whitespace()
         begin
@@ -137,19 +142,19 @@ module Parser
       name = xmlParseName()
       if (!name.nil? && name.downcase.slice(0..2) == "xml")
         if name.length > 3
-          raise XML_Reserved_Name, "Line #{@line}: Invalid name prefix 'xml'"
+          raise XML_Reserved_Name, "line #{@line}: Invalid name prefix 'xml'"
         else
-          raise XML_Reserved_Name, "Line #{@line}: XML declaration only allowed at the start of the document"
+          raise XML_Reserved_Name, "line #{@line}: XML declaration only allowed at the start of the document"
         end
       end
       
       if (name.include?(":"))
-        raise XML_Syntax_Error, "Line #{@line}: Colons are forbidden from PI names:\n #{@line}: '#{name}'"
+        raise XML_Syntax_Error, "line #{@line}: Colons are forbidden from PI names:\n #{@line}: '#{name}'"
       end
       return name
     end
     
-    # xmlParseName:
+    # xmlParseName
     # parse an XML name.
     #
     # [4] NameChar ::= Letter | Digit | '.' | '-' | '_' | ':' |
@@ -192,11 +197,31 @@ module Parser
     def xmlParseTextDecl()
       if (cmp("<?xml"))
         skip(5)
-        raise XML_Syntax_Error, "Line #{@line}: whitespace expected after '<?xml'" if !is_blank?
+        raise XML_Syntax_Error, "line #{@line}: whitespace expected after '<?xml'" if !is_blank?
+        
+        skip_blanks
+        version = xmlParseVersionInfo()
+        version = XML_DEFAULT_VERSION if version.nil?
+        raise XML_Syntax_Error, "line #{@line}: whitespace expected after version declaration" if !is_blank?
+        
+        encoding = xmlParseEncodingDecl()
+        raise XML_Syntax_Error, "line #{@line}: missing encoding in text declaration" if encoding.nil?
+        
+        skip_blanks
+        if (cmp('?>'))
+          skip(2)
+        elsif (cmp('>'))
+          raise XML_Syntax_Error, "xml declaration not finished"
+          skip
+        else
+          raise XML_Syntax_Error, "xml declaration not finished"
+          move_to_end_tag
+          skip
+        end
       end
     end
     
-    # xmlParseVersionInfo()
+    # xmlParseVersionInfo
     # parse the XML version
     #
     # [24] VersionInfo ::= S 'version' Eq (' VersionNum ' | " VersionNum ")
@@ -205,36 +230,130 @@ module Parser
     #
     # Returns the version string or nil
     def xmlParseVersionInfo()
+      skip_blanks
       version = nil
       if (cmp("version"))
         skip(7)
-        skip_blank
+        skip_blanks
         
-        raise XML_Syntax_Error, "Line #{@line}: '=' expected after 'version'" if (cur != '=')
+        raise XML_Syntax_Error, "line #{@line}: '=' expected after 'version'" if (cur != '=')
         skip # Skip the '='
-        skip_blank
+        skip_blanks
         
-        if (cur = "\"")
+        if (cur == "\"")
           skip # Skip the '\""
           version = xmlParseVersionNum()
-          raise XML_Syntax_Error, "Version number string not closed" if (cur != "\"")
+          raise XML_Syntax_Error, "version number string not closed" if (cur != "\"")
           skip
-        elsif (cur = "\'")
+        elsif (cur == "\'")
           skip # skip the "\'"
           version = xmlParseVersionNum()
-          raise XML_Syntax_Error, "Version number string not closed" if (cur != "\'")
+          raise XML_Syntax_Error, "version number string not closed" if (cur != "\'")
           skip
         else
-          raise XML_Syntax_Error, "Line #{@line}: Version number string not started, expected \" or \'"
+          raise XML_Syntax_Error, "line #{@line}: Version number string not started, expected \" or \'"
         end
       end
       return version
     end
     
+    # xmlParseVersionNum
+    # parse the XML version value.
+    # 
+    # [26] VersionNum ::= '1.' [0-9]+
+    # 
+    # In practice allow [0-9].[0-9]+ at that level
+    # 
+    # Returns the string giving the XML version number, or nil
     def xmlParseVersionNum()
+      version = ""
+      c = cur
+      if (!((c >= '0') && (c <= '9')))
+        raise XML_Syntax_Error, "line #{@line}: Major version number expected [0-9]"
+        return nil
+      end
+      version << c
+      skip # Skip the major version
+      
+      if (!cur == '.')
+        raise XML_Syntax_Error, "line #{@line}: '.' separator expected after major version"
+        return nil
+      end
+      version << '.'
+      skip # Skip the separator
+      
+      c = cur
+      while ((c >= '0') && (c <= '9')) # Snagging our minor version
+        version << c
+        skip
+        c = cur
+      end
+      return version
     end
     
+    # xmlParseEncodingDecl
+    # parse an XML encoding declaration
+    #
+    # [80] EncodingDecl ::= S 'encoding' Eq ('"' EncName '"' |  "'" EncName "'")
+    #
+    # TODO: This should set up conversion filters
+    #
+    # Returns the encoding value or nil
     def xmlParseEncodingDecl()
+      encoding = nil
+      skip_blanks
+      if (cmp("encoding"))
+        skip(8)
+        skip_blanks
+        
+        raise XML_Syntax_Error, "line #{@line}: '=' expected after 'encoding'" if (cur != '=')
+        skip
+        skip_blanks
+       
+        if (cur == "\"")
+          skip # Skip the '\""
+          encoding = xmlParseEncName()
+          raise XML_Syntax_Error, "encoding type string not closed" if (cur != "\"")
+          skip
+        elsif (cur == "\'")
+          skip # skip the "\'"
+          encoding = xmlParseEncName()
+          raise XML_Syntax_Error, "encoding type string not closed" if (cur != "\'")
+          skip
+        else
+          raise XML_Syntax_Error, "line #{@line}: encoding type string not started, expected \" or \'"
+        end
+        return encoding
+      end
+    end
+    
+    # xmlParseEncName
+    # parse the XML encoding name
+    #
+    # [81] EncName ::= [A-Za-z] ([A-Za-z0-9._] | '-')*
+    #
+    # Returns the encoding name or nil
+    def xmlParseEncName()
+      encoding = ""
+      
+      c = cur
+      
+      unless ((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z'))
+        raise XML_Syntax_Error, "line #{@line}: encoding name must begin with [a-zA-Z]"
+        return nil
+      end
+      
+      while (((c >= 'a') && (c <= 'z')) ||
+    	       ((c >= 'A') && (c <= 'Z')) ||
+    	       ((c >= '0') && (c <= '9')) ||
+    	       (c == '.') || (c == '_') ||
+    	       (c == '-')) do
+    	  
+    	  encoding << c
+    	  skip
+    	  c = cur
+    	end
+    	return encoding
     end
     
   end
