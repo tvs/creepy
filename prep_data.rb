9d857fdd1e6cbd755bac79ced62a370f2921f85d
Travis Hall <trvs.hll@gmail.com>
2010-10-26 23:26:21 -0700

    Script for prepping data for the indexer
    Prepends the name of the file into the file so that MapReduce passes
    along the necessary information for indexing. We need the filename
    in order to create the index, however MR would have simply sent the
    contents of the file. Note that this means the first line of each
    file should be treated as the document name ONLY, and then the
    subsequent lines used for indexing.
    
    Also note that this should be done by Creepy proper, else we add an
    unnecessary processing step before indexing. This script is
    predominantly for fixing up the files that already have been crawled.

---
+++ prep_data.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+require 'tempfile'
+require 'optparse'
+
+class File
+  def self.prepend(path, string)
+    Tempfile.open File.basename(path) do |tempfile|
+      tempfile << string
+
+      File.open(path, 'r+') do |file|
+        tempfile << file.read
+        file.pos = tempfile.pos = 0
+        file << tempfile.read
+      end
+    end
+  end
+
+  def self.prepend_line(path, string)
+    self.prepend(path, string + "\n");
+  end
+end
+
+optparse = OptionParser.new do |opts|
+  opts.banner = "Usage: prep_data.rb [options] <data_location>" 
+end
+optparse.parse!
+
+if ARGV.length == 0
+  puts optparse
+  exit
+end
+
+Dir.glob(ARGV[0]+'*').each do |file|
+  File.prepend_line(file, File.basename(file))
+end
